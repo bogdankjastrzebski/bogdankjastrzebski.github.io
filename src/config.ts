d151ea592e5d4ed312468eac77f55befd5a0415e
bogdankjastrzebski <bogdan.k.jastrzebski@gmail.com>
2025-03-15 20:01:02 +0100

    back

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,7 @@
 export const SITE_FAVICON = "/favicon.png";
 export const SITE_LOGO = "/avatar.png";
-export const SITE_TITLE = "Mia";
-export const SITE_DESCRIPTION =
-  "Mia is designed to be minimalist yet powerful, integrating the capabilities of Astro, MDX, and Rough Notation. Whether you're a developer, writer, or just someone who loves clean and efficient design, this theme is perfect for you.";
+export const SITE_TITLE = "Bogdan K. Jastrzębski";
+export const SITE_DESCRIPTION = "Bogdan K. Jastrzębski Web-Page";
 
 export const MENUS = [
   {
@@ -22,11 +21,11 @@ export const MENUS = [
   },
   {
     title: "GitHub",
-    link: "https://github.com/infinity-ooo/astro-theme-mia",
+    link: "https://github.com/bogdankjastrzebski",
     target: "_blank",
   },
 ];
 
-export const FOOTER_CONTENT = "Let's build a 1B unicorn 🦄";
+export const FOOTER_CONTENT = "";
 
 export const GOOGLE_GTAG = "G-XXXXXXXXXX";
